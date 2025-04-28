41289435fba51428701d4b46b5411193db1d4a94
TKKyuu <mapiapes@gmail.com>
2025-04-28 17:20:52 +0800

    Enhancing the code

--- app/src/main/java/com/example/habiaral/Fragment/ProgressBarFragment.java
+++ app/src/main/java/com/example/habiaral/Fragment/ProgressBarFragment.java
@@ -13,65 +13,71 @@ import com.example.habiaral.R;
 
 public class ProgressBarFragment extends Fragment {
 
-    private ProgressBar progressBar;
-    private TextView progressPercentageTextView;
+    private ProgressBar progressBarBahagi, progressBarKomprehensyon, progressBarKayarian;
+    private TextView progressPercentageBahagi, progressPercentageKomprehensyon, progressPercentageKayarian;
     private SharedPreferences sharedPreferences;
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        progressBar = view.findViewById(R.id.progressBar);
-        progressPercentageTextView = view.findViewById(R.id.progressPercentage);
+        progressBarBahagi = view.findViewById(R.id.progressBarBahagi);
+        progressPercentageBahagi = view.findViewById(R.id.progressPercentageBahagi);
+
+        progressBarKomprehensyon = view.findViewById(R.id.progressBarKomprehensyon);
+        progressPercentageKomprehensyon = view.findViewById(R.id.progressPercentageKomprehensyon);
+
+        progressBarKayarian = view.findViewById(R.id.progressBarKayarian);
+        progressPercentageKayarian = view.findViewById(R.id.progressPercentageKayarian);
+
         sharedPreferences = getActivity().getSharedPreferences("LessonProgress", getContext().MODE_PRIVATE);
 
         updateProgress();
     }
 
     private void updateProgress() {
-        boolean pangngalanDone = sharedPreferences.getBoolean("PangngalanDone", false);
-        boolean pandiwaDone = sharedPreferences.getBoolean("PandiwaDone", false);
-        boolean pangUriDone = sharedPreferences.getBoolean("PangUriDone", false);
-        boolean pangHalipDone = sharedPreferences.getBoolean("PangHalipDone", false);
-        boolean pangAbayDone = sharedPreferences.getBoolean("PangAbayDone", false);
-        boolean pangatnigDone = sharedPreferences.getBoolean("PangatnigDone", false);
-        boolean pangUkolDone = sharedPreferences.getBoolean("PangUkolDone", false);
-        boolean pangAkopDone = sharedPreferences.getBoolean("PangAkopDone", false);
-        boolean padamdamDone = sharedPreferences.getBoolean("PadamdamDone", false);
-        boolean pangawingDone = sharedPreferences.getBoolean("PangawingDone", false);
+        int bahagiCompleted = 0;
+        if (sharedPreferences.getBoolean("PangngalanDone", false)) bahagiCompleted++;
+        if (sharedPreferences.getBoolean("PandiwaDone", false)) bahagiCompleted++;
+        if (sharedPreferences.getBoolean("PangUriDone", false)) bahagiCompleted++;
+        if (sharedPreferences.getBoolean("PangHalipDone", false)) bahagiCompleted++;
+        if (sharedPreferences.getBoolean("PangAbayDone", false)) bahagiCompleted++;
+        if (sharedPreferences.getBoolean("PangatnigDone", false)) bahagiCompleted++;
+        if (sharedPreferences.getBoolean("PangUkolDone", false)) bahagiCompleted++;
+        if (sharedPreferences.getBoolean("PangAkopDone", false)) bahagiCompleted++;
+        if (sharedPreferences.getBoolean("PadamdamDone", false)) bahagiCompleted++;
+        if (sharedPreferences.getBoolean("PangawingDone", false)) bahagiCompleted++;
 
-        int completedLessons = 0;
-        if (pangngalanDone) completedLessons++;
-        if (pandiwaDone) completedLessons++;
-        if (pangUriDone) completedLessons++;
-        if (pangHalipDone) completedLessons++;
-        if (pangAbayDone) completedLessons++;
-        if (pangatnigDone) completedLessons++;
-        if (pangUkolDone) completedLessons++;
-        if (pangAkopDone) completedLessons++;
-        if (padamdamDone) completedLessons++;
-        if (pangawingDone) completedLessons++;
+        int bahagiTotal = 10;
+        int bahagiProgress = (bahagiCompleted * 100) / bahagiTotal;
+        progressBarBahagi.setProgress(bahagiProgress);
+        progressPercentageBahagi.setText(bahagiProgress + "%");
 
-        int totalLessons = 10;
-        int progressPercentage = (completedLessons * 100) / totalLessons;
+        int komprehensyonCompleted = 0;
+        if (sharedPreferences.getBoolean("Kwento1Done", false)) komprehensyonCompleted++;
+        if (sharedPreferences.getBoolean("Kwento2Done", false)) komprehensyonCompleted++;
+        if (sharedPreferences.getBoolean("Kwento3Done", false)) komprehensyonCompleted++;
 
-        progressBar.setProgress(progressPercentage);
+        int komprehensyonTotal = 3;
+        int komprehensyonProgress = (komprehensyonCompleted * 100) / komprehensyonTotal;
+        progressBarKomprehensyon.setProgress(komprehensyonProgress);
+        progressPercentageKomprehensyon.setText(komprehensyonProgress + "%");
 
-        progressPercentageTextView.setText(progressPercentage + "%");
+        int kayarianCompleted = 0;
+        if (sharedPreferences.getBoolean("PayakDone", false)) kayarianCompleted++;
+        if (sharedPreferences.getBoolean("TambalanDone", false)) kayarianCompleted++;
+        if (sharedPreferences.getBoolean("HugnayanDone", false)) kayarianCompleted++;
+        if (sharedPreferences.getBoolean("LangkapanDone", false)) kayarianCompleted++;
+
+        int kayarianTotal = 4;
+        int kayarianProgress = (kayarianCompleted * 100) / kayarianTotal;
+        progressBarKayarian.setProgress(kayarianProgress);
+        progressPercentageKayarian.setText(kayarianProgress + "%");
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_progress_bar, container, false);
-    }
-
-    public void onNextLessonButtonPressed(String lessonKey) {
-        SharedPreferences.Editor editor = sharedPreferences.edit();
-        editor.putBoolean(lessonKey, true);
-        editor.apply();
-
-        updateProgress();
     }
 
     @Override
