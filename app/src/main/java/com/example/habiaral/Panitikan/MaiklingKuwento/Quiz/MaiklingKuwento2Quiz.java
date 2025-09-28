8d475958d675b5c9644d713bea0d53061117de46
kyuu <mapiapes@gmai.com>
2025-09-28 21:01:36 +0800

    fix

--- app/src/main/java/com/example/habiaral/Panitikan/MaiklingKuwento/Quiz/MaiklingKuwento2Quiz.java
+++ app/src/main/java/com/example/habiaral/Panitikan/MaiklingKuwento/Quiz/MaiklingKuwento2Quiz.java
@@ -72,7 +72,7 @@ public class MaiklingKuwento2Quiz extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.panitikan_maikling_kwento2_quiz);
+        setContentView(R.layout.all_quiz_layout);
 
         AppPreloaderUtils.init(this);
 
@@ -85,12 +85,12 @@ public class MaiklingKuwento2Quiz extends AppCompatActivity {
         orangeDrawable = AppPreloaderUtils.orangeDrawable;
         greenDrawable = AppPreloaderUtils.greenDrawable;
 
-        questionTitle = findViewById(R.id.questionTitle);
-        questionText = findViewById(R.id.kwento4_questionText);
-        nextButton = findViewById(R.id.kwento4NextButton);
-        timerBar = findViewById(R.id.timerBar);
+        questionTitle = findViewById(R.id.question_number);
+        questionText = findViewById(R.id.question_text);
+        nextButton = findViewById(R.id.next_button);
+        timerBar = findViewById(R.id.timer_bar);
         introButton = findViewById(R.id.intro_button);
-        background = findViewById(R.id.bottomBar);
+        background = findViewById(R.id.bottom_bar);
 
         answer1 = findViewById(R.id.answer1);
         answer2 = findViewById(R.id.answer2);
