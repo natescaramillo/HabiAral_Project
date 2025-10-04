8f0f7fb2c14154e356525127ccb18982b7dc9989
kyuu <mapiapes@gmai.com>
2025-10-04 22:18:40 +0800

    fix

--- app/src/main/java/com/example/habiaral/Activity/AboutUsActivity.java
+++ app/src/main/java/com/example/habiaral/Activity/AboutUsActivity.java
@@ -1,17 +1,33 @@
 package com.example.habiaral.Activity;
 
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.widget.ImageView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.habiaral.BahagiNgPananalita.BahagiNgPananalita;
+import com.example.habiaral.BahagiNgPananalita.Lessons.PandiwaLesson;
+import com.example.habiaral.Fragment.SettingsFragment;
 import com.example.habiaral.R;
+import com.example.habiaral.Utils.SoundClickUtils;
 
 public class AboutUsActivity extends AppCompatActivity {
+
+    private ImageView btnBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.settings_about_us);
+
+        btnBack = findViewById(R.id.back_button);
+        btnBack.setOnClickListener(v -> {
+            SoundClickUtils.playClickSound(this, R.raw.button_click);
+            finish();
+        });
+
     }
 }
